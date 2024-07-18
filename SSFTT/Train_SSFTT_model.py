999867bac4a2baed3fcc126fadcfe3445757ed0c
HamzaElshafie <hamzaelshafie@outlook>
2024-07-18 22:55:54 +0100

    Added padWithZeros, createImageCubes, and splitTrainTestSet functions.

--- SSFTT/Train_SSFTT_model.py
+++ SSFTT/Train_SSFTT_model.py
@@ -17,12 +17,13 @@ import argparse
 import scipy.io as sio
 import SSFTTnet
 import get_cls_map
+from data_fetcher import loadData
+
+BATCH_SIZE_TRAIN = 64
 
 # Ensure the main directory is in the system path to import data_fetcher
 main_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(main_dir)
-
-from data_fetcher import loadData
 
 dataset_mapping = {
     'HanChuan': 'WHU-Hi-HanChuan',
@@ -44,6 +45,14 @@ def applyPCA(X, numComponents):
 
     return newX
 
+def padWithZeros(X, margin=2):
+    newX = np.zeros((X.shape[0] + 2 * margin, X.shape[1] + 2* margin, X.shape[2]))
+    x_offset = margin
+    y_offset = margin
+    newX[x_offset:X.shape[0] + x_offset, y_offset:X.shape[1] + y_offset, :] = X
+
+    return newX
+
 def create_data_loader():
     X, y = loadData()
     pca_components = 30
@@ -54,6 +63,32 @@ def create_data_loader():
     print('\n... ... PCA tranformation ... ...')
     X_pca = applyPCA(X, numComponents=pca_components)
     print('Data shape after PCA: ', X_pca.shape)
+
+def createImageCubes(X, y, windowSize=5, removeZeroLabels = True):
+    # Padding
+    margin = int((windowSize - 1) / 2)
+    zeroPaddedX = padWithZeros(X, margin=margin)
+    # Split patches
+    patchesData = np.zeros((X.shape[0] * X.shape[1], windowSize, windowSize, X.shape[2]))
+    patchesLabels = np.zeros((X.shape[0] * X.shape[1]))
+    patchIndex = 0
+    for r in range(margin, zeroPaddedX.shape[0] - margin):
+        for c in range(margin, zeroPaddedX.shape[1] - margin):
+            patch = zeroPaddedX[r - margin:r + margin + 1, c - margin:c + margin + 1]
+            patchesData[patchIndex, :, :, :] = patch
+            patchesLabels[patchIndex] = y[r-margin, c-margin]
+            patchIndex = patchIndex + 1
+    if removeZeroLabels:
+        patchesData = patchesData[patchesLabels>0,:,:,:]
+        patchesLabels = patchesLabels[patchesLabels>0]
+        patchesLabels -= 1
+
+    return patchesData, patchesLabels
+
+def splitTrainTestSet(X, y, testRatio, randomState=345):
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=testRatio, random_state=randomState, stratify=y)
+
+    return X_train, X_test, y_train, y_test
 
 
 if __name__ == "__main__":
