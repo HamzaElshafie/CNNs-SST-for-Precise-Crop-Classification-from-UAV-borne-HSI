01e68dfab868c558463f8d7bd544546c4cadbdd9
HamzaElshafie <hamzaelshafie@outlook>
2024-08-19 12:50:53 +0100

    .

--- HybridSN/HybridSN.py
+++ HybridSN/HybridSN.py
@@ -13,7 +13,7 @@ class HybridSN_network(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-        conv1_output_depth = (pca_components - 7) + 1 # Calculates the output depth after the 3D conv because there is no padding
+        conv1_output_depth = (pca_components - 3) + 1 # Calculates the output depth after the 3D conv because there is no padding
         print(f"Output dimension 1: {conv1_output_depth}")
         
         self.conv2 = nn.Sequential(
@@ -21,7 +21,7 @@ class HybridSN_network(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-        conv2_output_depth = (conv1_output_depth - 5) + 1 # Calculates the output depth after the 3D conv because there is no padding
+        conv2_output_depth = (conv1_output_depth - 3) + 1 # Calculates the output depth after the 3D conv because there is no padding
         print(f"Output dimension 2: {conv2_output_depth}")
 
         self.conv3 = nn.Sequential(
