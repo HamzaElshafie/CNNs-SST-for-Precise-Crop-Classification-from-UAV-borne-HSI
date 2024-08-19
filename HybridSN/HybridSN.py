464d0346792c66d6ded42561a823586bffb5ef8e
HamzaElshafie <hamzaelshafie@outlook>
2024-08-19 12:50:17 +0100

    .

--- HybridSN/HybridSN.py
+++ HybridSN/HybridSN.py
@@ -9,28 +9,37 @@ class HybridSN_network(nn.Module):
         super(HybridSN_network, self).__init__()
 
         self.conv1 = nn.Sequential(
-            nn.Conv3d(in_channels=1, out_channels=8, kernel_size=(7, 3, 3)),
+            nn.Conv3d(in_channels=1, out_channels=8, kernel_size=(3, 3, 7)),
             nn.ReLU(inplace=True)
         )
+
+        conv1_output_depth = (pca_components - 7) + 1 # Calculates the output depth after the 3D conv because there is no padding
+        print(f"Output dimension 1: {conv1_output_depth}")
         
         self.conv2 = nn.Sequential(
-            nn.Conv3d(in_channels=8, out_channels=16, kernel_size=(5, 3, 3)),
+            nn.Conv3d(in_channels=8, out_channels=16, kernel_size=(3, 3, 5)),
             nn.ReLU(inplace=True)
         )
-        
+
+        conv2_output_depth = (conv1_output_depth - 5) + 1 # Calculates the output depth after the 3D conv because there is no padding
+        print(f"Output dimension 2: {conv2_output_depth}")
+
         self.conv3 = nn.Sequential(
             nn.Conv3d(in_channels=16, out_channels=32, kernel_size=(3, 3, 3)),
             nn.ReLU(inplace=True)
         )
+
+        conv3_output_depth = (conv2_output_depth - 3) + 1 # Calculates the output depth after the 3D conv because there is no padding
+        print(f"Output dimension 3: {conv3_output_depth}")
         
         self.conv4 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3)),
+            nn.Conv2d(in_channels=32 * conv3_output_depth, out_channels=64, kernel_size=(3, 3)),
             nn.ReLU(inplace=True)
         )
         
         # Dummy input to calculate the size after conv4
         with torch.no_grad():
-            dummy_input = torch.randn(1, 1, pca_components, 30, 30)  # Example size, adapt if needed
+            dummy_input = torch.randn(1, 1, pca_components, 30, 30)  
             dummy_input = self.conv1(dummy_input)
             dummy_input = self.conv2(dummy_input)
             dummy_input = self.conv3(dummy_input)
@@ -68,11 +77,10 @@ class HybridSN_network(nn.Module):
         return out
 
 if __name__ == '__main__':
-    model = HybridSN_network(num_classes=NUM_CLASS, pca_components=30, dropout=0.1)
-    
+    pca_components = 30
+    model = HybridSN_network(num_classes=NUM_CLASS, pca_components=pca_components, dropout=0.1)
     # Create a dummy input tensor
-    dummy_input = torch.randn(1, 1, 30, 550, 400)  # Adjust the dimensions as needed
-    
-    # Forward pass through the network
-    output = model(dummy_input)
-    print(f"Model output shape: {output.shape}")
+    input = torch.randn(1, 1, pca_components, 550, 400)
+    y = model(input)
+    #summary(model, (1, img_rows, img_columns, band_dim))
+    print(y.size())
