240ae8968d4e132d0b26d9513df4539f7f8ea9fd
HamzaElshafie <hamzaelshafie@outlook>
2024-08-29 19:24:50 +0100

    Cleaned code.

--- DBDA/DBDAnet.py
+++ DBDA/DBDAnet.py
@@ -20,9 +20,6 @@ class PAM_Module(Module):
         super(PAM_Module, self).__init__()
         self.chanel_in = in_dim
 
-        # self.query_conv = Conv3d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
-        # self.key_conv = Conv3d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
-        # self.value_conv = Conv3d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
         self.query_conv = Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
         self.key_conv = Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
         self.value_conv = Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
@@ -31,30 +28,7 @@ class PAM_Module(Module):
         self.softmax = Softmax(dim=-1)
         
     def forward(self, x):
-        """
-            inputs :
-                x : input feature maps( B X C X H X W)
-            returns :
-                out : attention value + input feature
-                attention: B X (HxW) X (HxW)
-        """
-        # m_batchsize, channle, height, width, C = x.size()
-        #x = x.squeeze(-1)
         x = x.squeeze(2)
-        # m_batchsize, C, height, width, channle = x.size()
-
-        # proj_query = self.query_conv(x).view(m_batchsize, -1, width*height*channle).permute(0, 2, 1)
-        # proj_key = self.key_conv(x).view(m_batchsize, -1, width*height*channle)
-        # energy = torch.bmm(proj_query, proj_key)
-        # attention = self.softmax(energy)
-        # proj_value = self.value_conv(x).view(m_batchsize, -1, width*height*channle)
-        #
-        # out = torch.bmm(proj_value, attention.permute(0, 2, 1))
-        # out = out.view(m_batchsize, C, height, width, channle)
-        # print('out', out.shape)
-        # print('x', x.shape)
-
-        # m_batchsize, C, height, width = x.size()
         m_batchsize, C, height, width = x.size()
         proj_query = self.query_conv(x).view(m_batchsize, -1, width * height).permute(0, 2, 1)
         proj_key = self.key_conv(x).view(m_batchsize, -1, width * height)
@@ -78,19 +52,11 @@ class CAM_Module(Module):
 
         self.gamma = Parameter(torch.zeros(1))
         self.softmax  = Softmax(dim=-1)
+
     def forward(self,x):
-        """
-            inputs :
-                x : input feature maps( B X C X H X W)
-            returns :
-                out : attention value + input feature
-                attention: B X C X C
-        """
-        #m_batchsize, C, height, width, channle = x.size()
         m_batchsize, C, depth, height, width = x.size()
-        #print(x.size())
         proj_query = x.view(m_batchsize, C, -1)
-        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1) #形状转换并交换维度
+        proj_key = x.view(m_batchsize, C, -1).permute(0, 2, 1) 
         energy = torch.bmm(proj_query, proj_key)
         energy_new = torch.max(energy, -1, keepdim=True)[0].expand_as(energy)-energy
         attention = self.softmax(energy_new)
@@ -98,10 +64,8 @@ class CAM_Module(Module):
 
         out = torch.bmm(attention, proj_value)
         out = out.view(m_batchsize, C, depth, height, width)
-        # print('out', out.shape)
-        # print('x', x.shape)
 
-        out = self.gamma*out + x  #C*H*W
+        out = self.gamma*out + x  
         return out
 
 class DBDA_network_MISH(nn.Module):
@@ -114,71 +78,48 @@ class DBDA_network_MISH(nn.Module):
                                 kernel_size=(7, 1, 1), stride=(2, 1, 1))
         # Dense block
         self.batch_norm11 = nn.Sequential(
-                                    nn.BatchNorm3d(24,  eps=0.001, momentum=0.1, affine=True), # 动量默认值为0.1
-                                    #gelu_new()
-                                    #swish()
-            mish()
+                                    nn.BatchNorm3d(24,  eps=0.001, momentum=0.1, affine=True), mish()
         )
         self.conv12 = nn.Conv3d(in_channels=24, out_channels=12, padding=(3, 0, 0),
                                 kernel_size=(7, 1, 1), stride=(1, 1, 1))
         self.batch_norm12 = nn.Sequential(
-                                    nn.BatchNorm3d(36, eps=0.001, momentum=0.1, affine=True),
-                                    #gelu_new()
-                                    #swish()
-            mish()
+                                    nn.BatchNorm3d(36, eps=0.001, momentum=0.1, affine=True), mish()
         )
         self.conv13 = nn.Conv3d(in_channels=36, out_channels=12, padding=(3, 0, 0),
                                 kernel_size=(7, 1, 1), stride=(1, 1, 1))
         self.batch_norm13 = nn.Sequential(
-                                    nn.BatchNorm3d(48, eps=0.001, momentum=0.1, affine=True),
-                                    #gelu_new()
-                                    #swish()
-            mish()
+                                    nn.BatchNorm3d(48, eps=0.001, momentum=0.1, affine=True), mish()
         )
         self.conv14 = nn.Conv3d(in_channels=48, out_channels=12, padding=(3, 0, 0),
                                 kernel_size=(7, 1, 1), stride=(1, 1, 1))
         self.batch_norm14 = nn.Sequential(
-                                    nn.BatchNorm3d(60, eps=0.001, momentum=0.1, affine=True),
-                                    #gelu_new()
-                                    #swish()
-            mish()
+                                    nn.BatchNorm3d(60, eps=0.001, momentum=0.1, affine=True), mish()
         )
         kernel_3d = math.floor((band - 6) / 2) 
         print(f"Kernel_3d = {kernel_3d}")
         self.conv15 = nn.Conv3d(in_channels=60, out_channels=60,
-                                kernel_size=(kernel_3d, 1, 1), stride=(1, 1, 1)) # kernel size随数据变化
-
+                                kernel_size=(kernel_3d, 1, 1), stride=(1, 1, 1)) 
 
         # Spatial Branch
         self.conv21 = nn.Conv3d(in_channels=1, out_channels=24,
                                 kernel_size=(band, 1, 1), stride=(1, 1, 1))
         # Dense block
         self.batch_norm21 = nn.Sequential(
-                                    nn.BatchNorm3d(24, eps=0.001, momentum=0.1, affine=True),
-                                    #gelu_new()
-                                    #swish()
-            mish()
+                                    nn.BatchNorm3d(24, eps=0.001, momentum=0.1, affine=True), mish()
         )
         self.conv22 = nn.Conv3d(in_channels=24, out_channels=12, padding=(0, 1, 1),
                                 kernel_size=(1, 3, 3), stride=(1, 1, 1))
         self.batch_norm22 = nn.Sequential(
-                                    nn.BatchNorm3d(36, eps=0.001, momentum=0.1, affine=True),
-                                    #gelu_new()
-                                    #swish()
-            mish()
+                                    nn.BatchNorm3d(36, eps=0.001, momentum=0.1, affine=True), mish()
         )
         self.conv23 = nn.Conv3d(in_channels=36, out_channels=12, padding=(0, 1, 1),
                                 kernel_size=(1, 3, 3), stride=(1, 1, 1))
         self.batch_norm23 = nn.Sequential(
-                                    nn.BatchNorm3d(48, eps=0.001, momentum=0.1, affine=True),
-                                    #gelu_new()
-                                    #swish()
-            mish()
+                                    nn.BatchNorm3d(48, eps=0.001, momentum=0.1, affine=True), mish()
         )
         self.conv24 = nn.Conv3d(in_channels=48, out_channels=12, padding=(0, 1, 1),
                                 kernel_size=(1, 3, 3), stride=(1, 1, 1))
-
-
+        
         self.conv25 = nn.Sequential(
                                 nn.Conv3d(in_channels=1, out_channels=1, padding=(0, 1, 1),
                                 kernel_size=(2, 3, 3), stride=(1, 1, 1)),
@@ -186,17 +127,11 @@ class DBDA_network_MISH(nn.Module):
         )
 
         self.batch_norm_spectral = nn.Sequential(
-                                    nn.BatchNorm3d(60,  eps=0.001, momentum=0.1, affine=True), # 动量默认值为0.1
-                                    #gelu_new(),
-                                    #swish(),
-            mish(),
+                                    nn.BatchNorm3d(60,  eps=0.001, momentum=0.1, affine=True), mish(),
                                     nn.Dropout(p=0.5)
         )
         self.batch_norm_spatial = nn.Sequential(
-                                    nn.BatchNorm3d(60,  eps=0.001, momentum=0.1, affine=True), # 动量默认值为0.1
-                                    #gelu_new(),
-                                    #swish(),
-            mish(),
+                                    nn.BatchNorm3d(60,  eps=0.001, momentum=0.1, affine=True), mish(),
                                     nn.Dropout(p=0.5)
         )
 
@@ -210,41 +145,29 @@ class DBDA_network_MISH(nn.Module):
         self.attention_spectral = CAM_Module(60)
         self.attention_spatial = PAM_Module(60)
 
-        #fc = Dense(classes, activation='softmax', name='output1',
-        #           kernel_initializer=RandomNormal(mean=0.0, stddev=0.01))
 
     def forward(self, X):
         # spectral
         x11 = self.conv11(X)
-        #print('x11', x11.shape)
         x12 = self.batch_norm11(x11)
         x12 = self.conv12(x12)
-        #print('x12', x12.shape)
 
         x13 = torch.cat((x11, x12), dim=1)
-        #print('x13', x13.shape)
         x13 = self.batch_norm12(x13)
         x13 = self.conv13(x13)
-        #print('x13', x13.shape)
 
         x14 = torch.cat((x11, x12, x13), dim=1)
         x14 = self.batch_norm13(x14)
         x14 = self.conv14(x14)
 
         x15 = torch.cat((x11, x12, x13, x14), dim=1)
-        # print('x15', x15.shape)
-
         x16 = self.batch_norm14(x15)
         x16 = self.conv15(x16)
-        #print('x16', x16.shape)  # 7*7*97, 60
 
-        #print('x16', x16.shape)
-        # 光谱注意力通道
         x1 = self.attention_spectral(x16)
         x1 = torch.mul(x1, x16)
 
         # spatial
-        #print('x', X.shape)
         x21 = self.conv21(X)
         x22 = self.batch_norm21(x21)
         x22 = self.conv22(x22)
@@ -258,11 +181,7 @@ class DBDA_network_MISH(nn.Module):
         x24 = self.conv24(x24)
 
         x25 = torch.cat((x21, x22, x23, x24), dim=1)
-        #print('x25', x25.shape)
-        # x25 = x25.permute(0, 4, 2, 3, 1)
-        #print('x25', x25.shape)
 
-        # 空间注意力机制
         x2 = self.attention_spatial(x25)
         x2 = torch.mul(x2, x25)
 
@@ -275,15 +194,6 @@ class DBDA_network_MISH(nn.Module):
         x2 = x2.squeeze(-1).squeeze(-1).squeeze(-1)
 
         x_pre = torch.cat((x1, x2), dim=1)
-        #print('x_pre', x_pre.shape)
-
-        # model2
-        # x1 = torch.mul(x2, x16)
-        # x2 = torch.mul(x2, x25)
-        # x_pre = x1 + x2
-        #
-        #
         # x_pre = x_pre.view(x_pre.shape[0], -1)
         output = self.full_connection(x_pre)
-        # output = self.fc(x_pre)
         return output
